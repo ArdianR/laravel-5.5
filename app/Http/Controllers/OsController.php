f4b9b39fee6e36f4bf6eb0d1208bcf6d2ef1687a
ArdianR <fianr5750@gmail.com>
2017-11-28 18:53:23 +0700

    Initial commit

---
+++ app/Http/Controllers/OsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OsController extends Controller
+{
+    public function index()
+    {
+        return view('os');
+		
+    }
+}
